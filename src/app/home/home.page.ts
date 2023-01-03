2901630436e0cd7c652c9fca9f50c9f2150ecce4
Gerald Reimertz <gerald.reimertz@parcello.org>
2023-01-03 13:32:39 +0100

    RC0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  visible = false;
+  left = false;
+  timeout:any;
+
   constructor() {}
 
+  newCycle() {
+    // Generate a random number between 0 and 1
+    let flip = Math.random();
+
+    // Use the number to determine if it is left or right
+    if (flip < 0.5) {
+      this.left = true;
+    } else {
+      this.left = false;
+    }
+
+    // Make the decision visible, if it is not yet
+    if(!this.visible) {
+      this.visible = true;
+    }
+
+    // Clear the timeout, so that you cannot stack it by spamming the screen
+    if(this.timeout) {
+      clearTimeout(this.timeout);
+    }
+
+    // Set a timeout, so the decision hides after 2 Seconds
+    this.timeout = setTimeout(() =>{
+      this.visible = false;
+    }, 2000);
+
+  }
 }
